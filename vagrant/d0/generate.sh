77287220dd581a6ff2a7dae95025717257e89db3
Bert Raeymaekers <bert.raeymaekers@schilduil.org>
2018-11-08 10:31:54 +0100

    More work on testsusmapi, that also resulted in some changes in used roles.

--- vagrant/d0/generate.sh
+++ vagrant/d0/generate.sh
@@ -1,6 +1,7 @@
 source .token
 
-sed -e "s/\${MONGODB_CONNECTION_STRING}/$MONGODB_CONNECTION_STRING/" roles/testsusmapi/files/template_wsgi.py > roles/testsusmapi/files/wsgi.py
+# NOT USED ANYMORE, PASSES AS AN EXTRA_ARGS TO ANSIBLE.
+#sed -e "s/\${MONGODB_CONNECTION_STRING}/$MONGODB_CONNECTION_STRING/" roles/testsusmapi/files/template_wsgi.py > roles/testsusmapi/files/wsgi.py
 if [[ "$1" == "do" ]]
 then
     sed -e "s/\${DIGITAL_OCEAN_TOKEN}/$DIGITAL_OCEAN_TOKEN/" Vagrantfile.template > Vagrantfile
--- vagrant/d0/play.sh
+++ vagrant/d0/play.sh
@@ -1,1 +1,7 @@
-ansible-playbook playbook.yml
+source .token
+export ENV="DEV"
+if [ ! -z "$1" ]
+then
+    export ENV="$1"
+fi
+ansible-playbook -C -l $ENV playbook.yml -e "mongo_connection_string=${MONGODB_CONNECTION_STRING}"
