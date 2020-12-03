adca86e9f34c8b502c43efbd7aabcf2456bcd370
Bert Raeymaekers <bert.raeymaekers@schilduil.org>
2020-12-03 20:31:51 +0100

    FIX: If Kafka is strated too quickly after Zookeeper, Zookeeper isn't up yet.

---
+++ vagrant/kafka/bin/init_apps.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+for APP in 'init_messages'
+do
+
+    if [ ! -f ${DIR}/../app/${APP}/venv/bin/activate ]; then
+        cd ${DIR}/../app/${APP}
+        python3 -m venv venv
+    fi
+
+    source ${DIR}/../app/${APP}/venv/bin/activate
+    pip install --upgrade pip
+    pip install --upgrade -r ${DIR}/../app/${APP}/requirements.txt
+
+done
